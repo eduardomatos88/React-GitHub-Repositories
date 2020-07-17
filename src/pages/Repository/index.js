1a67c3ce1cdbb2d308a81ccd5f44955449fe89c8
Luiz Matos <luizmatos88@yahoo.com.br>
2020-07-16 22:12:36 -0300

    Adicionados Issues e usuários dessas issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-import { Loading } from './styles'
+import { Loading, Owner, IssueList } from './styles'
 import Container from '../../components/Container'
 import api from '../../services/api'
 
@@ -44,7 +45,32 @@ class Repository extends Component {
     if (loading) {
       return <Loading>Carregando</Loading>
     }
-    return <Container>Repository: </Container>
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issues.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    )
   }
 }
 export default Repository
